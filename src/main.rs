6d45053a164adf6b4ee1fc9e0c4557713285b759
David Wallace Croft <david@CroftSoft.com>
2024-09-08 16:04:07 -0500

    example 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use pattern_visitor::{visitor_0, visitor_2, visitor_3};
+use pattern_visitor::{visitor_0, visitor_1, visitor_3};
 
 fn main() {
   visitor_0::example();
 
-  visitor_2::example();
+  visitor_1::example();
 
   visitor_3::example();
 }
