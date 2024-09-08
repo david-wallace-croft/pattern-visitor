43852d6b1799dd54d15002a68b1a4d5240fea4e1
David Wallace Croft <david@CroftSoft.com>
2024-09-08 11:09:07 -0500

    Element

--- src/visitor_0/item.rs
+++ src/visitor_0/item.rs
@@ -1,36 +1,43 @@
 use super::value_item::ValueItem;
 use super::visitor::Visitor;
+use crate::visitor_0::element::Element;
 
 #[derive(Debug)]
 pub struct Item<T> {
   pub value: T,
 }
 
-impl ValueItem for Item<f64> {
+impl Element for Item<f64> {
   fn accept_visitor(&mut self, visitor: &dyn Visitor) {
     visitor.visit_item_f64(self);
   }
+}
 
+impl Element for Item<String> {
+  fn accept_visitor(&mut self, visitor: &dyn Visitor) {
+    visitor.visit_item_string(self);
+  }
+}
+
+impl Element for Item<usize> {
+  fn accept_visitor(&mut self, visitor: &dyn Visitor) {
+    visitor.visit_item_usize(self);
+  }
+}
+
+impl ValueItem for Item<f64> {
   fn double_value(&mut self) {
     self.value *= 2.;
   }
 }
 
 impl ValueItem for Item<String> {
-  fn accept_visitor(&mut self, visitor: &dyn Visitor) {
-    visitor.visit_item_string(self);
-  }
-
   fn double_value(&mut self) {
     self.value = self.value.repeat(2);
   }
 }
 
 impl ValueItem for Item<usize> {
-  fn accept_visitor(&mut self, visitor: &dyn Visitor) {
-    visitor.visit_item_usize(self);
-  }
-
   fn double_value(&mut self) {
     self.value *= 2;
   }
--- src/visitor_0/mod.rs
+++ src/visitor_0/mod.rs
@@ -2,29 +2,38 @@ mod item;
 mod visitor;
 mod half_value_visitor;
 mod value_item;
+mod element;
 
 use self::half_value_visitor::HalfValueVisitor;
 use self::item::Item;
 use self::value_item::ValueItem;
+use crate::visitor_0::element::Element;
 
 pub fn example() {
   let mut item_f64 = Item::<f64> { value: 1. };
   let mut item_string = Item::<String> { value: "1".to_string() };
   let mut item_usize = Item::<usize> { value: 1 };
 
-  let mut items: Vec<&mut dyn ValueItem> = vec![
+  let mut value_items: Vec<&mut dyn ValueItem> = vec![
     &mut item_usize,
     &mut item_f64,
     &mut item_string,
   ];
 
-  println!("{items:?}");
+  println!("{value_items:?}");
 
-  items.iter_mut().for_each(|item| item.double_value());
+  value_items.iter_mut().for_each(|value_item| value_item.double_value());
 
-  println!("{items:?}");
+  println!("{value_items:?}");
 
-  items.iter_mut().for_each(|item| item.accept_visitor(&HalfValueVisitor));
+  let mut elements: Vec<&mut dyn Element> = vec![
+    &mut item_usize,
+    &mut item_f64,
+    &mut item_string,
+  ];
 
-  println!("{items:?}");
+  elements.iter_mut().for_each(|element|
+    element.accept_visitor(&HalfValueVisitor));
+
+  println!("{elements:?}");
 }
