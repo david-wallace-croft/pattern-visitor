ee26fa4329ce788d53207f00780395d8fef37be2
David Wallace Croft <david@CroftSoft.com>
2024-09-08 08:11:43 -0500

    iterator

---
+++ src/visitor_0/item.rs
@@ -0,0 +1,6 @@
+#[derive(Debug)]
+pub struct Item {
+  pub health: isize,
+  pub wealth: f64,
+  pub wisdom: usize,
+}
--- src/visitor_0/mod.rs
+++ src/visitor_0/mod.rs
@@ -1,3 +1,13 @@
+mod item;
+
+use item::Item;
+
+const ITEM_0: Item = Item { health: 0, wealth: 0., wisdom: 0 };
+const ITEM_1: Item = Item { health: 1, wealth: 1., wisdom: 1 };
+const ITEM_2: Item = Item { health: 2, wealth: 2., wisdom: 2 };
+
 pub fn example() {
-  println!("Hello, world!");
+  let items: Vec<Item> = vec![ITEM_0, ITEM_1, ITEM_2];
+
+  items.iter().for_each(|item| println!("item: {item:?}"));
 }
