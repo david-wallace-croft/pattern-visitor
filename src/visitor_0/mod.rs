558333c924e21e9942c29738ffd3d318313f9218
David Wallace Croft <david@CroftSoft.com>
2024-09-08 11:58:47 -0500

    Box

--- src/visitor_0/mod.rs
+++ src/visitor_0/mod.rs
@@ -10,22 +10,16 @@ use self::generic_element::GenericElement;
 use self::half_value_visitor::HalfValueVisitor;
 
 pub fn example() {
-  let mut item_0 = GenericElement::<usize> {
-    value: 1,
-  };
-
-  let mut item_1 = GenericElement::<f64> {
-    value: 1.,
-  };
-
-  let mut item_2 = GenericElement::<String> {
-    value: "1".to_string(),
-  };
-
-  let mut elements: Vec<&mut dyn Element> = vec![
-    &mut item_0,
-    &mut item_1,
-    &mut item_2,
+  let mut elements: Vec<Box<dyn Element>> = vec![
+    Box::new(GenericElement::<usize> {
+      value: 1,
+    }),
+    Box::new(GenericElement::<f64> {
+      value: 1.,
+    }),
+    Box::new(GenericElement::<String> {
+      value: "1".to_string(),
+    }),
   ];
 
   println!("{elements:?}");
